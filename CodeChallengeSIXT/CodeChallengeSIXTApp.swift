93124eeb664b7f9dd2c2ebac547b98ba6ea8fb06
mspanadgoo <mspanadgoo@me.com>
2022-06-20 15:48:10 +0430

    Project Created.

---
+++ CodeChallengeSIXT/CodeChallengeSIXTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallengeSIXTApp.swift
+//  CodeChallengeSIXT
+//
+//  Created by Mohammad Sadegh Panadgoo on 3/30/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct CodeChallengeSIXTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
